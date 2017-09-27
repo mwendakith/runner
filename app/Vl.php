1dcb5d0c0fd8c694ca5c85b2c077ea87a3529263
joelkith <joel.kithinji@strathmore.edu>
2017-09-27 12:52:54 +0300

    Suppression 4

--- app/Vl.php
+++ app/Vl.php
@@ -898,12 +898,12 @@ class Vl extends Model
 				$suppression = ($suppressed * 100) / $tests;
 			}
 
-			$data_array = array('facility' => $value->ID, 'dateupdated' => $today,
-			'suppressed' => $suppressed, 'nonsuppressed' => $nonsuppressed, 'suppression' => $suppression);
-			// $data_array = array('dateupdated' => $today,'suppressed' => $suppressed, 'nonsuppressed' => $nonsuppressed, 'suppression' => $suppression);
+			// $data_array = array('facility' => $value->ID, 'dateupdated' => $today,
+			// 'suppressed' => $suppressed, 'nonsuppressed' => $nonsuppressed, 'suppression' => $suppression);
+			$data_array = array('dateupdated' => $today,'suppressed' => $suppressed, 'nonsuppressed' => $nonsuppressed, 'suppression' => $suppression);
 
-			DB::table('vl_site_suppression')->insert($data_array);
-			// DB::table('vl_site_suppression')->where('facility', $value->ID)->update($data_array);
+			// DB::table('vl_site_suppression')->insert($data_array);
+			DB::table('vl_site_suppression')->where('facility', $value->ID)->update($data_array);
 		}
 
     	echo "\n Completed entry into vl suppression at " . date('d/m/Y h:i:s a', time()); 
