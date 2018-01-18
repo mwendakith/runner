689b7f706750dc4acc8affce76d817fe6cde8d5b
mwendakith <joelkith@gmail.com>
2018-01-18 17:05:20 +0300

    Facility lab mapping

--- app/Eid.php
+++ app/Eid.php
@@ -808,11 +808,11 @@ class Eid extends Model
 	    		foreach ($counties as $county) {
 	    			$search = ['month' => $month, 'lab' => $lab->ID, 'county' => $county->ID];
 	    			$find = ['month' => $month, 'lab' => $lab->ID, 'county' => $county->ID, 'year' => $year];
-	    			$tests = $this->checknull( $tests_a->where($search) );
+	    			$tests = $this->checknull( $tests_a->where('month', $month)->where('lab', $lab->ID)->where('county', $county->ID) );
 	    			if($tests == 0){
 	    				continue;
 	    			}
-	    			$supported = $this->checknull($supported_sites_a->where($search));
+	    			$supported = $this->checknull( $supported_sites_a->where('month', $month)->where('lab', $lab->ID)->where('county', $county->ID) );
 
 	    			$data_array = ['total' => $tests, 'site_sending' => $supported];
 
