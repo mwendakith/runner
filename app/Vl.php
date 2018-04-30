2d0aff0a479747c53c891cfc1d5290ed910aa3ca
mwendakith <joelkith@gmail.com>
2018-04-30 15:53:11 +0300

    Run 2017

--- app/Vl.php
+++ app/Vl.php
@@ -1012,7 +1012,7 @@ class Vl
     	$updates = 0;
     	$column2 = $column;
 
-    	for ($type=3; $type < 4; $type++) { 
+    	for ($type=1; $type < 7; $type++) { 
 
     		// if($type == 3 && $column == "facility"){
 			// 	continue;
@@ -1248,7 +1248,7 @@ class Vl
 			$array_size++;
 		}
 
-		echo $this->finish_division($start_month, $year, $today, $div_array, 'facility', 'viralsamples.facility', 4, $array_size);
+		// echo $this->finish_division($start_month, $year, $today, $div_array, 'facility', 'viralsamples.facility', 4, $array_size);
 		return $this->finish_facilities_regimen($start_month, $year, $today, $div_array, 'facility', 'viralsamples.facility', 4, $array_size);
     }
 
