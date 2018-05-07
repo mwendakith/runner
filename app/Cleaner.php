4c3eb9ec9a7c0d688a92a2a4fa9f9380d177a051
mwendakith <joelkith@gmail.com>
2018-05-07 12:26:28 +0300

    Change

--- app/Cleaner.php
+++ app/Cleaner.php
@@ -23,7 +23,8 @@ class Cleaner
 		// return null;
 
 		foreach ($indices as $key => $ind) {
-			$sql = "DROP INDEX `" . $ind->index_name . "` ON " . $ind->table_name . ";";
+			// $sql = "DROP INDEX `" . $ind->index_name . "` ON " . $ind->table_name . ";";
+			$sql = "ALTER TABLE " . $ind->table_name . "DROP INDEX `" . $ind->index_name . "`;";
 			DB::statement($sql);
 		}
 	}
