15349bb83e3b2ec9282e4e5cd9789e0368a5a297
Joel <joelkith@gmail.com>
2019-10-15 12:53:35 +0300

    Updates

--- app/V2/Eid.php
+++ app/V2/Eid.php
@@ -1146,6 +1146,7 @@ class Eid
 
 			// Get collection instances of the data
 	    	$alltests_a = $n->CumulativeTestedSamples($year, $month, $division);
+	    	dd($alltests_a);
 	    	$eqatests_a = $n->OverallEQATestedSamples($year, $month, $division);
 
 	    	$tests_a = $n->OverallTestedSamples($year, $month, $division);
